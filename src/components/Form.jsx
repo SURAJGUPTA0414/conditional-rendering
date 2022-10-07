59f7a3fd126413fef665806a9e03e43e036d1d7e
SURAJGUPTA0414 <sandygupta0414@gmail.com>
2022-10-07 14:02:07 +0530

    Initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Form(props) {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      {props.userIsRegistered ===false?<input type="password" placeholder="Confirm Password" />:null}
+      
+      <button type="submit">{props.userIsRegistered?"Login":"Register"}</button>
+    </form>
+  );
+}
+
+export default Form;
